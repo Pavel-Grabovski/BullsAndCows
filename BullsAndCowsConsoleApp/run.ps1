503cc4f5e8c8988707a39b8ed9b12a2c3bc9d8de
Pavel Grabovskiy <Araa9977@mail.ru>
2025-02-11 00:28:25 +0700

    docker run -d --name bullsandcowsconsoleappcontainer bullsandcowsconsoleapp

--- BullsAndCowsConsoleApp/run.ps1
+++ BullsAndCowsConsoleApp/run.ps1
@@ -1,1 +1,2 @@
 docker build -f "./Dockerfile" --force-rm -t bullsandcowsconsoleapp  --build-arg "BUILD_CONFIGURATION=Release" "C:\Users\Home\Source\Repos\BullsAndCows"
+docker run -d --name bullsandcowsconsoleappcontainer bullsandcowsconsoleapp
