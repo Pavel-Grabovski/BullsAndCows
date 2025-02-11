5149e5ada3e82c3e6517cf3c36a4bc59feb61476
PavelGrabovskiy <Araa9977@mail.ru>
2025-02-11 16:51:47 +0700

    Добавил удаление контейнера в run.ps

--- BullsAndCowsConsoleApp/run.ps1
+++ BullsAndCowsConsoleApp/run.ps1
@@ -1,2 +1,7 @@
-docker build -f "./Dockerfile" --force-rm -t bullsandcowsconsoleapp  --build-arg "BUILD_CONFIGURATION=Release" "C:\Users\Home\Source\Repos\BullsAndCows"
+$path = Get-Location
+$parentFolder = Split-Path -Path $path -Parent
+
+docker rm -f bullsandcowsconsoleappcontainer
+
+docker build -f "./Dockerfile" --force-rm -t bullsandcowsconsoleapp  --build-arg "BUILD_CONFIGURATION=Release" $parentFolder
 docker run -d --name bullsandcowsconsoleappcontainer bullsandcowsconsoleapp
